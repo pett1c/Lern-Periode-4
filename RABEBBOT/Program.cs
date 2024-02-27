435e0c96d06992c08e85c0874d2b345d6f85bbc0
Igor <igor.tyenyetilov@gmail.com>
2024-02-27 11:00:57 +0100

    yes.

---
+++ RABEBBOT/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace RABEBBOT
+
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var client = new TelegramBotClient("7099629430:AAE9nsg7Wd3DIkCzYUzLbfoxMNg2-IF-RwE");
+            client.StartReceiving(Update, Error);
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                Console.WriteLine($"{message.Chat.FirstName} | {message.Text}");
+                if (message.Text.Equals("/start"))
+                {
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Привет, меня зовут RABEB, и я помогу тебе с настройкой твоего графика дня. Как к тебе обращаться?");
+                }
+            }
+        }
+
+        private static Task Error(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
+
+//else if (message.Text.ToLower().Contains("..."))
+//{
+//    await botClient.SendTextMessageAsync(message.Chat.Id, "Доброго! Я бот, который поможет тебе с найстрокой распорядка дня. ");
+//    return;
+//}
---
+++ catdei/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+public class Bot
+{
+    private static readonly string BotToken = "7099629430:AAE9nsg7Wd3DIkCzYUzLbfoxMNg2-IF-RwE";
+    private static readonly long ChatId = -1001610516293;
+    private static readonly string[] MondayFolder = Directory.GetFiles("Monday");
+    private static readonly string[] FridayFolder = Directory.GetFiles("Friday");
+    private static readonly string[] RandomFolder = Directory.GetFiles("Random");
+    private static readonly Random random = new Random();
+    private static DateTime lastRandomSent = DateTime.Now;
+
+    public static void Main(string[] args)
+    {
+        var botClient = new TelegramBotClient(BotToken);
+
+        botClient.OnMessage += BotOnMessage;
+        botClient.StartReceiving();
+
+        Console.WriteLine("Бот запущен.");
+
+        while (true)
+        {
+            CheckDayAndSendImage(botClient);
+            CheckRandomImageCooldownAndSend(botClient);
+            Thread.Sleep(1000);
+        }
+    }
+
+    private static void BotOnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+    {
+        if (e.Message.Text == "/start")
+        {
+            botClient.SendTextMessageAsync(ChatId, "Привет! Я бот для отправки картинок.");
+        }
+    }
+
+    private static void CheckDayAndSendImage(TelegramBotClient botClient)
+    {
+        var dayOfWeek = DateTime.Now.DayOfWeek;
+        string[] folder;
+
+        switch (dayOfWeek)
+        {
+            case DayOfWeek.Monday:
+                folder = MondayFolder;
+                break;
+            case DayOfWeek.Friday:
+                folder = FridayFolder;
+                break;
+            default:
+                return;
+        }
+
+        var image = GetRandomImage(folder);
+        botClient.SendPhotoAsync(ChatId, image);
+    }
+
+    private static void CheckRandomImageCooldownAndSend(TelegramBotClient botClient)
+    {
+        if ((DateTime.Now - lastRandomSent).TotalHours >= 4)
+        {
+            var image = GetRandomImage(RandomFolder);
+            botClient.SendPhotoAsync(ChatId, image);
+            lastRandomSent = DateTime.Now;
+        }
+    }
+
+    private static string GetRandomImage(string[] folder)
+    {
+        return folder[random.Next(folder.Length)];
+    }
+}
+
+
+// private static readonly string BotToken = "7099629430:AAE9nsg7Wd3DIkCzYUzLbfoxMNg2-IF-RwE";
+// private static readonly long ChatId = -1001610516293;
